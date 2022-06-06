c716d185b038d783e349a4bd3146225edfe7d1e1
bananeira <ijamil.osoria@gmail.com>
2022-06-06 17:08:43 +0200

    added provisionally tests to main

--- methodical-megalodon/src/Main.java
+++ methodical-megalodon/src/Main.java
@@ -1,5 +1,23 @@
+import codewars.assignment.HumanReadableDurationFormat;
+import codewars.assignment.IsNumberAPrime;
+import codewars.assignment.TortoiseRacing;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(HumanReadableDurationFormat.formatDuration(852734));
+
+        System.out.println(Arrays.toString(new boolean[]{
+                IsNumberAPrime.isPrime(27)
+            }
+        ));
+
+        System.out.println(Arrays.toString(new boolean[]{
+                IsNumberAPrime.isPrime(3)
+            }
+        ));
+
+        System.out.println(Arrays.toString(TortoiseRacing.race(750, 820, 90)));
     }
 }
