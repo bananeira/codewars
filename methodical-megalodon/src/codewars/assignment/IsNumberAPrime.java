001a65e64c4b0602f7cf4db852a4d67246a67c59
Jamil Osoria <51927662+bananeira@users.noreply.github.com>
2022-06-08 15:11:16 +0200

    Update IsNumberAPrime.java

--- methodical-megalodon/src/codewars/assignment/IsNumberAPrime.java
+++ methodical-megalodon/src/codewars/assignment/IsNumberAPrime.java
@@ -2,15 +2,11 @@ package codewars.assignment;
 
 public class IsNumberAPrime {
     public static boolean isPrime(int num) {
-        if (num <= 1)
-            return false;
-
-        if (num <= 3)
-            return true;
+        if (num <= 1) return false;
+        if (num <= 3) return true;
 
         for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0)
-                return false;
+            if (num % i == 0) return false;
         }
 
         return true;
