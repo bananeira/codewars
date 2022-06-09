96d0695d0e40e009a1a5a8d3e5ba59d612ff520f
Jamil Osoria <51927662+bananeira@users.noreply.github.com>
2022-06-09 16:02:19 +0200

    Update HumanReadableTime.java

--- methodical-megalodon/src/codewars/assignment/HumanReadableTime.java
+++ methodical-megalodon/src/codewars/assignment/HumanReadableTime.java
@@ -3,25 +3,22 @@ package codewars.assignment;
 import java.text.DecimalFormat;
 
 public class HumanReadableTime {
-    public static String makeReadable(int seconds) {
+    public static String makeReadable(int sec) {
         DecimalFormat formatter = new DecimalFormat("00");
 
-        if ( 0 > seconds || seconds > 359999 ) {
+        if ( 0 > sec || sec > 359999 ) {
             return "no. why?";
         }
 
         return String.format("%s:%s:%s",
-            formatter.format(seconds / 3600),
-            formatter.format(seconds / 60 % 60),
-            formatter.format(seconds % 60));
+            formatter.format(sec / 3600),
+            formatter.format(sec / 60 % 60),
+            formatter.format(sec % 60));
     }
 
-    public static String makeReadableShort(int seconds) {
-        return ( seconds >= 0 && seconds < 359999 )
-            ? String.format("%02d:%02d:%02d",
-            seconds / 3600,
-            seconds / 60 % 60,
-            seconds % 60)
+    public static String makeReadableShort(int sec) {
+        return ( sec >= 0 && sec < 359999 )
+            ? String.format("%02d:%02d:%02d", sec / 3600, sec / 60 % 60, sec % 60) 
             : "no. why?";
     }
 }
